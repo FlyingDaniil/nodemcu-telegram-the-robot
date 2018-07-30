4db191e9cbaf2df1b27e3860739ad10d9657605f
zhucharych <zhuk914@gmail.com>
2018-07-31 01:31:57 +0300

    ,

--- lesson_09/lesson_09.ino
+++ lesson_09/lesson_09.ino
@@ -1,16 +1,20 @@
+//файл с настройками сети и токеном бота
 #include "secrets.h"
 
+//подключение библиотек
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <TelegramBot.h>
 #include <SFE_BMP180.h>
 #include <Wire.h>
 
+//объявление бота и датчика
 WiFiClientSecure net_ssl;
 TelegramBot bot (BotToken, net_ssl);
 SFE_BMP180 pressure;
 
 void setup() {
+  //подключение wifi, бота, датчика
   Serial.begin(9600);
   wifiConnect();
   bot.begin(); 
@@ -18,13 +22,17 @@ void setup() {
 }
 
 void loop() {
+  //получение сообщения
   message m = bot.getUpdates(); 
-  
-  if (m.text.equals("Pressure")){     
+
+  //если запрос давления
+  if (m.text.equals("Pressure")){   
+    //то отправляем данные  
     bot.sendMessage(m.chat_id, String(round(getPressure()))+" Pa");  
   }
 }
 
+//функция для подключения к wifi
 void wifiConnect(){
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -36,6 +44,7 @@ void wifiConnect(){
   Serial.println(WiFi.localIP());
 }
 
+//функция для получения давления
 double getPressure() {
   char status;
   double T, P, p0, a;
